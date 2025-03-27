3b6b63b21a86e16f4121a1c1878ca1bdaac8747b
TitoCalleros <mirna.molina@gmail.com>
2025-03-27 13:37:42 -0600

    Implementación de un navbar de DaisyUI obteniendo las rutas y el título para escribirlo dinámicamente desde la ruta elegida.

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
+import { filter, map, tap } from 'rxjs';
+
+import { routes } from '../../../app.routes';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [AsyncPipe, RouterLink],
   templateUrl: './navbar.component.html'
 })
-export class NavbarComponent { }
+export class NavbarComponent {
+  router = inject(Router);
+
+  routes = routes.map( (route) => ({
+    path: route.path,
+    title: `${route.title ?? 'Mapas en Angular'}`,
+  })).filter ( r => r.path !== '**');
+
+  // El postfijo $ sirve para identificar variables de observables
+  pageTitle$ = this.router.events.pipe(
+    filter ((event) => event instanceof NavigationEnd),
+    map ( (event) => event.url),
+    map ( url => routes.find( route => `/${ route.path }` === url)?.title ?? 'Mapas')
+  );
+
+}
