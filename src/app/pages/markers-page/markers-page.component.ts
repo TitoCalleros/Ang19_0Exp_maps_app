4b5c1208ab1030d5a1c6d015c0cd2b48d14ee7a5
TitoCalleros <mirna.molina@gmail.com>
2025-04-03 12:46:35 -0600

    Creación de una interface para manejar el listado de marcadores.
    Se crea función para agregar marcadores al hacer clic. Se agrega cada marcador creado al listado.

--- src/app/pages/markers-page/markers-page.component.ts
+++ src/app/pages/markers-page/markers-page.component.ts
@@ -1,9 +1,15 @@
 import { AfterViewInit, Component, ElementRef, signal, viewChild } from '@angular/core';
 import mapboxgl from 'mapbox-gl';
 import { environment } from '../../../environments/environment';
-import { JsonPipe } from '@angular/common';
+import { v4 as uuidv4 } from 'uuid';
 
 mapboxgl.accessToken = environment.mapboxKey;
+
+interface Marker {
+  id: string;
+  mapboxMarker: mapboxgl.Marker;
+}
+
 
 @Component({
   selector: 'app-markers-page',
@@ -13,6 +19,7 @@ mapboxgl.accessToken = environment.mapboxKey;
 export class MarkersPageComponent implements AfterViewInit {
   divElement = viewChild<ElementRef>('map');
   map = signal<mapboxgl.Map | null>(null);
+  markers = signal<Marker[]>([]);
 
   async ngAfterViewInit() {
     if (!this.divElement()?.nativeElement) return;
@@ -28,22 +35,46 @@ export class MarkersPageComponent implements AfterViewInit {
       zoom: 14, // starting zoom
     });
 
-    const newMarker = new mapboxgl.Marker({
-      color: 'red',
-      draggable: false,
-      scale: 1,
-    }).setLngLat([-99.1906, 18.9685]).addTo(map);
+    // const newMarker = new mapboxgl.Marker({
+    //   color: 'red',
+    //   draggable: false,
+    //   scale: 1,
+    // }).setLngLat([-99.1906, 18.9685]).addTo(map);
 
-    newMarker.on('dragend', (event) => {
-      console.log(event.target);
+    // newMarker.on('dragend', (event) => {
+    //   console.log(event.target);
 
-    });
+    // });
 
     this.mapListeners(map);
   }
 
   mapListeners(map: mapboxgl.Map) {
-    console.log('object');
+    map.on('click', (event ) => this.addMarker(event));
 
+    this.map.set(map);
+  }
+
+  addMarker(event: mapboxgl.MapMouseEvent) {
+
+    if (!this.map()) return;
+
+    const map = this.map()!;
+
+    const color = '#xxxxxx'.replace(/x/g, (y) =>
+      ((Math.random() * 16) | 0).toString(16)
+    );
+
+    const mapboxMarker = new mapboxgl.Marker({
+      color: color
+    }).setLngLat(event.lngLat).addTo(map);
+
+    const newMarker: Marker = {
+      id: uuidv4(),
+      mapboxMarker: mapboxMarker
+    };
+
+    // this.markers.set([newMarker, ...this.markers()]);
+    this.markers.update(markers => [newMarker, ...markers]);
   }
 }
